96932b2828af45937338f5b44fa576d1b742b02f
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2023-10-16 10:18:00 +0800

    fix:修复python导包路径替换表达式

--- rpc/build.sh
+++ rpc/build.sh
@@ -4,4 +4,4 @@ protoc -I=proto \
     --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
     proto/*.proto
 python3.9 -m grpc_tools.protoc -I=proto --python_out=python/pb --grpc_python_out=python/pb  proto/*.proto
-sed -i 's/import \(.*\) as/from . import \1 as/g' ./python/pb/*.py*
+sed -i '/from google\.protobuf.* import \(.*\) as/! s/import \(.*\) as/from . import \1 as/g' ./python/pb/*.py*
